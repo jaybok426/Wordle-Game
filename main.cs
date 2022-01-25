5c9375b113f10c554073942e3a8dc176611d8130
Jay Bok <94381222+jaybok426@users.noreply.github.com>
2022-01-25 16:00:50 +0000

    Commit and push

--- main.cs
+++ main.cs
@@ -2,6 +2,33 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World!");
+    //Pick the word to guess
+    string computerWord = "HELLO";
+    int Guesses = 5;
+    bool WonGame = false;
+    //Read in the user's word
+    do{
+    Console.WriteLine("Guess the word");
+    string UserhasGuess = Console.ReadLine();
+    Guesses--;
+    if (Guesses == 0){
+      Console.WriteLine("Out of Guesses You have Lost");
+    }
+    //Go through each letter
+      //if letter is in word?
+      if (UserhasGuess == computerWord){
+        //if letter is in right place?
+        Console.WriteLine("You have won");
+        WonGame = true;
+      }
+      if (WonGame == true){
+      System.Environment.Exit(0); 
+    }
+          //print "G"
+        //else
+          //print "Y"
+      //else 
+        //print"B"
+    }while (Guesses>0);
   }
 }
