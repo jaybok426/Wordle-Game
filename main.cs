f162cdfaa154da7d2b7d20ed0e639d06f4ed96b3
Jay Bok <94381222+jaybok426@users.noreply.github.com>
2022-01-28 15:11:37 +0000

    Made it work

--- main.cs
+++ main.cs
@@ -3,11 +3,18 @@ using System;
 class Program {
   public static void Main (string[] args) {
     //Pick the word to guess
-    string computerWord = "HELLO";
+    char FirstLetter = 'H';
+    char SecondLetter = 'E';
+    char ThirdLetter = 'L';
+    char ForthLetter = 'L';
+    char FifthLetter = 'O';
+    string computerWord = ""+FirstLetter+SecondLetter+ThirdLetter+ForthLetter+FifthLetter;
     int Guesses = 5;
     bool WonGame = false;
+    int NumberOfRightLetters = 0;
     //Read in the user's word
     do{
+      NumberOfRightLetters = 0;
     Console.WriteLine("Guess the word");
     string UserhasGuess = Console.ReadLine();
     Guesses--;
@@ -16,12 +23,37 @@ class Program {
     }
     //Go through each letter
       //if letter is in word?
-      if (UserhasGuess == computerWord){
+      if (UserhasGuess[0] == FirstLetter){
         //if letter is in right place?
-        Console.WriteLine("You have won");
-        WonGame = true;
+        Console.WriteLine("G");
+        NumberOfRightLetters += 1;
+        if (UserhasGuess[1] == SecondLetter){
+        //if letter is in right place?
+        Console.WriteLine("G");
+        NumberOfRightLetters += 1;
+        }
+        if (UserhasGuess[2] == ThirdLetter){
+        //if letter is in right place?
+        Console.WriteLine("G");
+        NumberOfRightLetters += 1;
+        }
+        if (UserhasGuess[3] == ForthLetter){
+        //if letter is in right place?
+        Console.WriteLine("G");
+        NumberOfRightLetters += 1;
+        }
+        if (UserhasGuess[4] == FifthLetter){
+        //if letter is in right place?
+        Console.WriteLine("G");
+        NumberOfRightLetters += 1;
+        }
+        //WonGame = true;
       }
+       if (NumberOfRightLetters == 5){
+      WonGame = true; 
+    }
       if (WonGame == true){
+       Console.WriteLine("You have won");
       System.Environment.Exit(0); 
     }
           //print "G"
